2a9e11f6595e8f363d5536fd619856f88454e709
jprocasky <jeff@breathermae.com>
2026-07-02 17:28:58 -0500

    Update class-bmf-pillars-saver.php

--- breathermae-forms/includes/class-bmf-pillars-saver.php
+++ breathermae-forms/includes/class-bmf-pillars-saver.php
@@ -138,12 +138,7 @@ if (!class_exists('BMF_Pillars_Saver')) {
             if (!$row_id) return false;
 
             $wpdb->query($wpdb->prepare(
-                "UPDATE {$t_res}
-                    SET 'rank' = %s,
-                        updated_at = NOW()
-                  WHERE id = %d AND user_email = %s AND current_flag = 1",
-                $rank_string, $row_id, $email
-            ));
+                "UPDATE {$t_res} SET `rank` = %s,  updated_at = NOW() WHERE id = %d AND user_email = %s AND current_flag = 1", $rank_string, $row_id, $email ));
 
             self::maybe_finalize($user_id, $row_id);
             return true;
