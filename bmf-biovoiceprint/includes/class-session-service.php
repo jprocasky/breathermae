8c952177322433fc02e043e0c9e05b539b2c3eb8
jprocasky <jeff@breathermae.com>
2026-08-03 17:28:39 -0500

    conflicts

--- bmf-biovoiceprint/includes/class-session-service.php
+++ bmf-biovoiceprint/includes/class-session-service.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * BioVoicePrint - Session / group business logic.
@@ -623,6 +622,3 @@ class BMF_BioVoice_Session_Service {
 		];
 	}
 }
-=======
-PLACEHOLDER_REPLACE_WITH_FULL_CONTENT
->>>>>>> a3a6c58872922f7c8db86d47408c4506536c7e56
